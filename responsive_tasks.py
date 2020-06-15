b1beb49910347ab5037ae60c8d239c7db9d2a1a2
The-Earth <tigerzeng97@gmail.com>
2020-06-15 23:48:01 +0800

    [Modify](responsive_tasks.py) Add support of cmds like /cmd@name

--- responsive_tasks.py
+++ responsive_tasks.py
@@ -6,11 +6,12 @@ config = json.load(open('config.json', 'r', encoding='utf-8'))
 bot = catbot.Bot(config)
 
 
-def get_id_cri(msg: catbot.Message) -> bool:
-    return '/user_id' in msg.commands
+def user_id_cri(msg: catbot.Message) -> bool:
+    match = '/user_id'
+    return match in msg.commands or f'{match}@{bot.username}' in msg.commands
 
 
-def get_id(msg: catbot.Message):
+def user_id(msg: catbot.Message):
     from_id = msg.from_.id
     chat_id = msg.chat.id
     reply_to = msg.id
@@ -18,7 +19,8 @@ def get_id(msg: catbot.Message):
 
 
 def get_chat_id_cri(msg: catbot.Message) -> bool:
-    return '/chat_id' in msg.commands
+    match = '/chat_id'
+    return match in msg.commands or f'{match}@{bot.username}' in msg.commands
 
 
 def get_chat_id(msg: catbot.Message):
@@ -28,7 +30,7 @@ def get_chat_id(msg: catbot.Message):
 
 
 if __name__ == '__main__':
-    bot.add_task(get_id_cri, get_id)
+    bot.add_task(user_id_cri, user_id)
     bot.add_task(get_chat_id_cri, get_chat_id)
 
     bot.start()
