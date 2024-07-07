5cc215884fb7970c8c1decc2738915125f2e68ac
The-Earth <26201356+The-Earth@users.noreply.github.com>
2024-07-07 12:30:05 +0800

    [Fix] Update to catbot-1.0.0
    Fix stop signal handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
 from multiprocessing import Event
+from threading import Thread
 
 from components import bot
 from components.new_pages import new_pages_main
@@ -9,20 +9,20 @@ from components.spam_detection import porn_detect_main
 
 def bot_run(stop_event: Event):
     with bot:
-        bot.start(stop_event)
+        bot.start(stop_event, timeout=5, print_log=True)
+    return
 
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
     event = Event()
-    scheduled_stop_poll_p = multiprocessing.Process(target=stop_poll_scheduled_main, args=(event,))
+    scheduled_stop_poll_p = Thread(target=stop_poll_scheduled_main, args=(event,))
     scheduled_stop_poll_p.start()
-    bot_p = multiprocessing.Process(target=bot_run, args=(event,))
+    bot_p = Thread(target=bot_run, args=(event,))
     bot_p.start()
-    new_pages_p = multiprocessing.Process(target=new_pages_main, args=(event,))
+    new_pages_p = Thread(target=new_pages_main, args=(event,))
     new_pages_p.start()
-    # porn_detect_p = multiprocessing.Process(target=porn_detect_main, daemon=True)
-    # porn_detect_p.start()
+    porn_detect_p = Thread(target=porn_detect_main, daemon=True)
+    porn_detect_p.start()
 
     import time
     while True:
